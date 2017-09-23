8dfa50cd5e561179f006484e4f936ab94721b80b
RapidTheNerd <george.marr.cobc@gmail.com>
2017-09-23 16:04:32 +0100

    made it readonly

--- UsageExample.cs
+++ UsageExample.cs
@@ -4,8 +4,8 @@ namespace HypixelCSHARP
 {
     public class UsageExample
     {
-        private string player = HypixelAPI.getPlayerByName("RapidTheNerd");
-        private string playerBoosters = HypixelAPI.getBoostersByUuid("43db704e-10b1-40b3-a38d-ce059de35a59");
+        private readonly string player = HypixelAPI.getPlayerByName("RapidTheNerd");
+        private readonly string playerBoosters = HypixelAPI.getBoostersByUuid("43db704e-10b1-40b3-a38d-ce059de35a59");
         
         private void print()
         {
