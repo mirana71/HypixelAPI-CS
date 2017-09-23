c7e05824698bae3edbf265dcd1f56e5163c14c54
RapidTheNerd <george.marr.cobc@gmail.com>
2017-09-23 16:03:21 +0100

    added example

---
+++ UsageExample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HypixelCSHARP
+{
+    public class UsageExample
+    {
+        private string player = HypixelAPI.getPlayerByName("RapidTheNerd");
+        private string playerBoosters = HypixelAPI.getBoostersByUuid("43db704e-10b1-40b3-a38d-ce059de35a59");
+        
+        private void print()
+        {
+            Console.Write(player.ToString());
+            Console.Write(playerBoosters.ToString());
+        }
+    }
+   
+}
